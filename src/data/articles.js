412e24f4c9b395a6451cd5adf0f995f4e5ab61dd
Marina Lendt <marinalendt@gmail.com>
2025-11-28 15:13:50 +0100

    add: articles

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,8 +8,8 @@ export const articles = [
   {
     image: ArticleImage1,
     date: "February 24th",
-    title: "Passion",
-    text: "Will I ever find my one true passion again? text text text",
+    title: "Where is the passion?",
+    text: "From sports fields to lines of code. I’m a former nurse and elite athlete turned Frontend Developer student. I thrive on teamwork, problem-solving, and building creative, user-centered web solutions. Currently exploring HTML, CSS, JavaScript, React, and Node.js — always learning and growing in tech.",
     link: "https://www.linkedin.com/posts/marina-lendt-1b3439194_fr%C3%A5n-idrottsplanen-till-kodraderna-activity-7395022889470656512-2L0E?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC2gYNcBFQ9RdRRKm6_ec4kEU-b_foJs9Q8"
 
   },
@@ -17,17 +17,17 @@ export const articles = [
   {
     image: ArticleImage2,
     date: "July 14th",
-    title: "Life",
-    text: "text text text text",
-    link: "#"
+    title: "Teamwork and Growth in the Workplace",
+    text: "Teamwork, trust, and growth. Working closely with dedicated colleagues in healthcare taught me the value of teamwork, clear communication, and trust. I thrive and develop best when I feel supported and can rely on the people around me — principles I now carry with me into web development, where collaboration and shared purpose drive both learning and success.",
+    link: "https://narrow-composer-f16.notion.site/Teamwork-and-Growth-in-the-Workplace-2b9c059d9f1480cc9e90e31f5bab4013?source=copy_link"
 
   },
 
   {
     image: ArticleImage3,
-    date: "August 26th",
-    title: "Web-development",
-    text: "text text text text",
-    link: "#"
+    date: "November 9th",
+    title: "Starting Over: Embracing a New Career in Tech",
+    text: "Embracing a new beginning. Changing careers after nearly 18 years in the same field was both challenging and exciting. Starting from scratch in web development has tested me in many ways, but it’s also incredibly rewarding. Every day brings opportunities to learn, solve problems, and grow — and I’m just getting started on this new journey!",
+    link: "https://narrow-composer-f16.notion.site/Starting-Over-Embracing-a-New-Career-in-Tech-2b9c059d9f1480b6ba79cbba9750e201?source=copy_link"
   }
 ];
